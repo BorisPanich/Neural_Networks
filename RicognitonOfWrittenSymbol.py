c117c3d8a19874bcdd242c3a8abc53f02a387a6c
BorisPanich <panich2303@gmail.com>
2022-06-10 07:03:28 +0300

    Распознование письменных цифр

---
+++ RicognitonOfWrittenSymbol.py
@@ -0,0 +1,90 @@
+''' Распознование письменных цифр '''
+
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import mnist  # библиотека базы выборок (рукописных цифр) Mnist
+from tensorflow import keras
+from tensorflow.keras.layers import Dense, Flatten
+
+# загрузка обучающей и тестовой выборок
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# нормализация входных данных
+x_train = x_train / 255
+x_test = x_test / 255
+
+# преобразование выходных значений в векторы по категориям
+y_train_cat = keras.utils.to_categorical(y_train, 10)
+y_test_cat = keras.utils.to_categorical(y_test, 10)
+
+# отображение первых 25 изображений из обучающей выборки
+plt.figure(figsize=(10, 5))
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(x_train[i], cmap=plt.cm.binary)
+
+plt.show()
+
+# формирование модели НС и вывод ее структуры в консоль
+
+model = keras.Sequential([
+    Flatten(input_shape=(28, 28, 1)),  # преобразование изобр. в 28х28 Пикселей
+    Dense(128, activation='relu'),  # функция активации скрытого слоя 
+    Dense(10, activation='softmax')  # функция выходного слоя
+])
+
+print(model.summary())  # вывод структуры НС в консоль
+
+# Компиляция НС с оптимизацией по Adam и критерием-категориальная кросс-энтропия
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])  # процент правильнораспознаных цифр
+
+# Запуск процесса обучения: 80% - обучающая выборка, 20% - выборка валидации (validation_split)
+model.fit(x_train, y_train_cat, batch_size=32, epochs=5, validation_split=0.2)
+
+model.evaluate(x_test, y_test_cat)
+
+# Проверка распознования цифр
+n = 1
+x = np.expand_dims(x_test[n], axis=0)
+res = model.predict(x)
+print(res)
+print(np.argmax(res))
+
+plt.imshow(x_test[n], cmap=plt.cm.binary)
+plt.show()
+
+# Распознавание всей тестовой выборки
+pred = model.predict(x_test)
+pred = np.argmax(pred, axis=1)
+
+print(pred.shape)
+
+print(pred[:20])
+print(y_test[:20])
+
+# Выделение неверных вариантов
+mask = pred == y_test
+print(mask[:10])
+
+x_false = x_test[~mask]
+y_false = x_test[~mask]
+
+print(x_false.shape)
+
+# Вывод первых 25 неверных результатов
+plt.figure(figsize=(10, 5))
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(x_false[i], cmap=plt.cm.binary)
+
+plt.show()
